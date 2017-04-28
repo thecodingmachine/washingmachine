7df608fe1e2e45b3b813beed918f5b8ccba9a9ec
David Négrier <d.negrier@thecodingmachine.com>
2017-04-28 13:29:58 +0200

    Fixing merge-base git command

--- src/Git/GitRepository.php
+++ src/Git/GitRepository.php
@@ -8,7 +8,7 @@ class GitRepository extends \Cz\Git\GitRepository
 {
     public function getMergeBase(string $commit1, string $commit2) : string
     {
-        $results = $this->extractFromCommand('git merge-base' . escapeshellarg($commit1). ' '. escapeshellarg($commit2));
+        $results = $this->extractFromCommand('git merge-base ' . escapeshellarg($commit1). ' '. escapeshellarg($commit2));
 
         return $results[0];
     }
