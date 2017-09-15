29876b4024668347cab266025829f45c3ab5b4fc
David Négrier <d.negrier@thecodingmachine.com>
2017-09-15 12:06:36 +0200

    Debugging finding common ancestor

--- src/Git/GitRepository.php
+++ src/Git/GitRepository.php
@@ -4,10 +4,21 @@
 namespace TheCodingMachine\WashingMachine\Git;
 
 
+use Cz\Git\GitException;
+
 class GitRepository extends \Cz\Git\GitRepository
 {
     public function getMergeBase(string $commit1, string $commit2) : string
     {
+        try {
+            $this->extractFromCommand('git merge-base --is-ancestor ' . escapeshellarg($commit1) . ' ' . escapeshellarg($commit2));
+        } catch (GitException $e) {
+            // The command will return exit code 1 if $commit1 is an ancestor of $commit2
+            // Exit code one triggers an exception. We catch it.
+            return $commit1;
+        }
+
+
         $results = $this->extractFromCommand('git merge-base ' . escapeshellarg($commit1). ' '. escapeshellarg($commit2));
 
         return $results[0];
